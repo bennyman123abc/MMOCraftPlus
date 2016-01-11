5c1e627a47337895ab645d957a25be3862287dd4
bennyman123abc <bennyman123abc@hotmail.com>
2016-01-11 17:30:39 -0500

    Exp from Monsters and Animals
    Beginning of the code where players get Exp when killing a monster or
    animal. The stats for Exp and Levels are implemented into the
    Events.java but NOT OFFICIALLY IMPLEMENTED INTO THE PLUGIN (yet)

--- src/com/ziangames/Events.java
+++ src/com/ziangames/Events.java
@@ -1,6 +1,10 @@
 package com.ziangames;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class Events implements Listener{
 	
@@ -9,6 +13,20 @@ public class Events implements Listener{
 	public Events(PluginStart plugin) {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		config = plugin;
+	}
+	
+	@EventHandler
+	public void playerKilled(EntityDeathEvent e){
+		Entity killer = e.getEntity();
+		
+		if(killer instanceof Player) {
+			Player player = ((Player) killer).getPlayer();
+			
+			int oldExp = config.getConfig().getInt(player + ".exp");
+			int playerLevel = config.getConfig().getInt(player + ".lvl");
+			
+			
+		}
 	}
 
 }
